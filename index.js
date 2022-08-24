2bf1b3c8ce87e88dd771a4342a2aaf80280ac88c
rafi993 <rafi993@protonmail.com>
2022-08-24 21:49:35 +0530

    Fixing path

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const React = require("react");
 const { render } = require("./reactSong");
 const importJsx = require("import-jsx");
 
-const ui = importJsx("./mary_had_a_little_lamb");
+const ui = importJsx("./examples/mary_had_a_little_lamb");
 
 render(React.createElement(ui), "test.midi");
