0cd5eb8dea717e68658d690afb138cb7b809566c
rafi993 <rafi993@protonmail.com>
2022-08-25 08:26:45 +0530

    Fixing tone

--- examples/electronic.js
+++ examples/electronic.js
@@ -5,17 +5,8 @@ const GuitarBeats = ({ repeat }) => {
   return (
     <>
       <track channel={4} repeat={repeat} instrument="electronicGuitar">
-        <note name="B1" />
-        <note name="B2" />
         <note name="c1" />
-        <note name="c2" />
-        <note name="c3" />
         <note name="c4" />
-        <note name="c5" />
-        <note name="g1" />
-        <note name="g2" />
-        <note name="g3" />
-        <note name="g4" />
       </track>
     </>
   );
